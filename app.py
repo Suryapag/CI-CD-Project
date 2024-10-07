03afeb51768914f68bf589e90100bb3b0e19258e
Suryapag <suryapa9092@gmail.com>
2024-10-07 13:38:12 +0530

    Auto-commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import platform
 
 app = Flask(__name__)
 
+network_metrics={}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -50,6 +52,7 @@ def monitor():
             return jsonify({"status": "down", "host": host, "message": "Host is not reachable"}), 200
         else:
             return jsonify({"status": "up", "host": host, "response_time": response_time}), 200
+        
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -73,6 +76,11 @@ def traceroute():
             return jsonify({"status": "error", "error_message": result.stderr}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+# API endpoint to get stored metrics
+@app.route('/metrics', methods=['GET'])
+def get_metrics():
+    return jsonify(network_metrics), 200  # Return the stored metrics
     
 
 if __name__ == '__main__':
